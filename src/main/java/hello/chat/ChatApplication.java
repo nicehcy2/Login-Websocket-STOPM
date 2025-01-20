c41f54d2f1a591c32e49e232e4e5c7931eca072e
nicehcy <nicehcy2@naver.com>
2025-01-20 21:35:21 +0900

    feat: rabbitmq 설정

--- src/main/java/hello/chat/ChatApplication.java
+++ src/main/java/hello/chat/ChatApplication.java
@@ -3,6 +3,7 @@ package hello.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ChatApplication {
 
