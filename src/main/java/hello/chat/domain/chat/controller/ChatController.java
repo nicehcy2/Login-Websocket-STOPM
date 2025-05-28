6230d5bec551c99f4b69c6a42215461d3e6c28ec
nicehcy <nicehcy2@naver.com>
2025-05-28 23:08:44 +0900

    DELETE: ACK 로직 삭제(사유는 주석에 작성)

--- src/main/java/hello/chat/domain/chat/controller/ChatController.java
+++ src/main/java/hello/chat/domain/chat/controller/ChatController.java
@@ -58,12 +58,24 @@ public class ChatController {
     /**
      * 클라이언트로부터 메시지를 정상적으로 수신했는지 확인하는 메서드
      * 클라이언트가 정상적으로 메시지를 수신했다면, Redis DB에 저장
+     *
+     * 그런데 Redis에 변경된 내용을 저장하는 과정에서 문제가 생길 가능성 떄문에 일단은 없애기로 결정
+     *
+     * 이걸 없애도 되는 이유가 outbox 테이블을 사용했고 이것 때문에 outbox 테이블을 적용한 의미가 없어지기 때문.
+     * 전송이 실패하더라도 전송 시도를 해서 서버로 보낼 경우 DB 쪽에서 문제(즉, 저장이 되지 않는)가 발생하지 않는한
+     * 동기화 과정에서 채팅 내역을 복구할 수 있기에
+     *
+     * 채팅 전송 과정에서 문제 시 채팅이 실시간 전송이 되지 않고, 알림도 가지 않지만
+     * 네트워크 환경에서 어쩔 수 없는 상황이기에 서버 관리에 신경을 쓰자.
      */
+    /*
     @MessageMapping("chat.message.ack")
     public void acknowledgeMessage(@RequestBody MessageDto messageDto) {
 
         messageService.saveMessage(messageDto);
     }
+
+     */
 
     // 메시지 읽음 처리
     // fixme: 근데 이거 잘하면 얘도 웹소켓으로 처리해도 될듯? 프론트랑 상의 필요
